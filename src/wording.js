1d68c79251dd0ff0387ae6820e585837703cf2bd
byjuw <julien.wachter@gmail.com>
2022-12-29 17:11:11 +0100

    wording: Update description wording

--- src/wording.js
+++ src/wording.js
@@ -1,34 +1,32 @@
 const wording = {
   en: {
-    description: `I am a french lead developer. Passionate, I like to create and to learn new things.
+    description: `I am a french developer. Passionate, I like to create and learn new things.
 
-    Since 5 years I work for <a href="https://sopheos.com/" target="_blank">Sopheos</a>, a French company building high traffic web and mobile applications.
+    For 5 years, I worked at <a href="https://sopheos.com/" target="_blank">Sopheos</a>, a French company building high traffic web and mobile applications.
+    Since september 2022, I am a Senior Software Engineer at <a href="https://botify.com" target="_blank">Botify</a> a SEO platform.
 
     Outside of programming, I enjoy badminton, <a href="https://www.worldcubeassociation.org/persons/2009WACH02" target="_blank">Rubik's cube</a> and have recently started playing chess.
 
-    I am discreet on the Internet, but you can find me on <a href="https://github.com/byjuw" target="_blank">Github</a>, <a href="https://twitter.com/WachterJulien" target="_blank">Twitter</a> and <a href="https://www.linkedin.com/in/julien-wachter/" target="_blank">LinkedIn</a>.
-
-    You can also contact me by e-mail at julien(at this domain).
-    My resume (french) is available <a href="/resume.pdf" target="_blank">here</a>.`,
+    I am discreet on the Internet, but you can find me on <a href="https://github.com/byjuw" target="_blank">Github</a> or <a href="https://www.linkedin.com/in/julien-wachter/" target="_blank">LinkedIn</a>.
+    You can also contact me by e-mail at julien(at this domain).`,
     lang: 'English',
     portrait: 'picture of Julien Wachter',
-    profession: 'Lead developer',
+    profession: 'Senior Software Engineer',
     theme: 'Theme',
   },
   fr: {
-    description: `Je suis un lead développeur français. Passionné, j'aime créer et apprendre de nouvelles choses.
+    description: `Je suis un développeur français. Passionné, j'aime créer et apprendre de nouvelles choses.
 
-    Depuis 5 ans, je travaille pour <a href="https://sopheos.com/" target="_blank">Sopheos</a>, une entreprise française qui développe des applications web et mobile à fort trafic.
+    Pendant 5 ans, j'ai travaillé chez <a href="https://sopheos.com/" target="_blank">Sopheos</a>, une entreprise française qui développe des applications web et mobile à fort trafic.
+    Depuis septembre 2022, je suis développeur senior chez <a href="https://botify.com" target="_blank">Botify</a>, une plateforme de SEO.
 
     En dehors du développement, j'aime le badminton, le <a href="https://www.worldcubeassociation.org/persons/2009WACH02" target="_blank">Rubik's cube</a> et j'ai récemment commencé à jouer aux échecs.
 
-    Je suis discret sur Internet, mais vous pouvez me trouver sur <a href="https://github.com/byjuw" target="_blank">Github</a>, <a href="https://twitter.com/WachterJulien" target="_blank">Twitter</a> et <a href="https://www.linkedin.com/in/julien-wachter/" target="_blank">LinkedIn</a>.
-
-    Vous pouvez également me contacter par e-mail à julien(arobase ce domaine).
-    Mon cv est disponible <a href="/resume.pdf" target="_blank">ici</a>.`,
+    Je suis discret sur Internet, mais vous pouvez me trouver sur <a href="https://github.com/byjuw" target="_blank">Github</a> ou <a href="https://www.linkedin.com/in/julien-wachter/" target="_blank">LinkedIn</a>.
+    Vous pouvez également me contacter par e-mail à julien(arobase ce domaine).`,
     lang: 'Français',
     portrait: 'photo de Julien Wachter',
-    profession: 'Lead développeur',
+    profession: 'Développeur senior',
     theme: 'Thème',
   },
 }
